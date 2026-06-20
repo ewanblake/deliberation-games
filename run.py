64a8c192dda39a854ccd5e321204d69d184773a9
Ewan Blake <ewanblake02@gmail.com>
2026-06-20 19:32:49 +0100

    Integrating skeleton of dialogue engine into Python Flask

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
+from app.dialogue.engine import DialogueEngine
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Deliberation Games"
+
+@app.route("/simulate")
+def simulate():
+
+    engine = DialogueEngine()
+
+    engine.run()
+
+    return "Simulation complete!"
 
 if __name__ == "__main__":
     app.run(debug=True)
