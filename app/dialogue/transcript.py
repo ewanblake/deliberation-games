86e23ccffe4c320fc48d48a5a321e457d1355b13
Ewan Blake <ewanblake02@gmail.com>
2026-06-22 15:53:23 +0100

    Formatting JSON data for recording each turn

--- app/dialogue/transcript.py
+++ app/dialogue/transcript.py
@@ -6,3 +6,21 @@ class TranscriptManager:
     def __init__(self):
 
         self.turns == []
+
+    def record_turn(
+            self,
+            turn,
+            agent,
+            state,
+            move,
+            proposal=None,
+            target_proposal=None
+    ):
+        turn_data = {
+            "turn": turn,
+            "agent": agent,
+            "state": state,
+            "move": move,
+            "proposal": proposal,
+            "target_proposal": target_proposal
+        }
