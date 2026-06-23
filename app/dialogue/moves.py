9ad014c0c8508190c9f39d0c7ea00c596b276159
Ewan Blake <ewanblake02@gmail.com>
2026-06-23 19:27:25 +0100

    Adding additional move types

--- app/dialogue/moves.py
+++ app/dialogue/moves.py
@@ -2,5 +2,8 @@ from enum import Enum
 
 class MoveType(Enum):
     PROPOSE = "PROPOSE"
+    SUPPORT = "SUPPORT"
+    CHALLENGE = "CHALLENGE"
     ACCEPT = "ACCEPT"
     REJECT = "REJECT"
+    WITHDRAW = "WITHDRAW"
